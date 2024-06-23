4ffdc7d751518a1805853d132f76271f30e49ddf
Fernando Peres Esteves <zed10.business@gmail.com>
2024-06-23 19:41:12 -0300

    Revert "testar o source control git integrado no vscode"
    This reverts commit e6401c9163ec2c807266c35f96cff7a020ac8b79.

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 let msg = prompt('Caso/motivo de consulta: ');
 let ticket = Math.random() * 30 + 1;
 console.log(`Motivo: ${msg} | Ticket (na fila): ${ticket}`);
-//testando o source control com o git integrado no vscode (fazer controle de versoes do codigo)

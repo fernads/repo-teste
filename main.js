e6401c9163ec2c807266c35f96cff7a020ac8b79
Fernando Peres Esteves <zed10.business@gmail.com>
2024-06-23 19:31:48 -0300

    testar o source control git integrado no vscode

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 let msg = prompt('Caso/motivo de consulta: ');
 let ticket = Math.random() * 30 + 1;
 console.log(`Motivo: ${msg} | Ticket (na fila): ${ticket}`);
+//testando o source control com o git integrado no vscode (fazer controle de versoes do codigo)

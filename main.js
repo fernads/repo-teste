333b09f183e652887fa935d37078af297b5efa04
Fernando Peres Esteves <zed10.business@gmail.com>
2024-06-23 21:29:01 -0300

    commit para resetar

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-let msg = prompt('Caso/motivo de consulta: ');
-let ticket = Math.random() * 30 + 1;
-console.log(`Motivo: ${msg} | Ticket (na fila): ${ticket}`);
+console.log('Programa de consulta iniciado');
+console.log('---- Processo de Tickets ----')
+
+function consultaMotTicket() {
+    let msg = prompt('Caso/motivo de consulta: ');
+    console.log(`${msg} -> motivo de consulta`);
+    let ticket = parseInt(Math.random() * 30 + 1);
+    console.log(`${ticket} -> senha/ticket em fila`);
+    alert(`Motivo: ${msg} | Ticket (na fila): ${ticket}`);    
+}
+
+consultaMotTicket();
+
+let pacientes = 1;
+while (pacientes <= 30) {
+    consultaMotTicket();
+    pacientes++;
+}

753c1533d7570d85542285cf0461a6caf4650507
Fernando Peres Esteves <zed10.business@gmail.com>
2024-06-23 21:37:35 -0300

    Adicionar console.log para indicar testes

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+console.log('Aplicativo rodando');
+console.log('TicketConsulta V1.0 - Beta')
+
 let msg = prompt('Caso/motivo de consulta: ');
 let ticket = Math.random() * 30 + 1;
 console.log(`Motivo: ${msg} | Ticket (na fila): ${ticket}`);
